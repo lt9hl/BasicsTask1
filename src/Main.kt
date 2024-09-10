b2b65efefe4ca8f6763f0a2efb97ace64c2204ec
chepilev <positivecookie@mail.ru>
2024-09-10 12:41:40 +0300

    12345

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,24 @@
 fun main() {
-    println("Hello Worfvfvdvdfvdfvfdvld!")
+        print("Введите последовательность символов: ")
+        val name = readln()?.toCharArray()
+        name?.let {
+            var countName: String = ""
+            var count: Int = 1;
+            for (i in 0..name.size - 1) {
+                count = 1
+                if (((0 <= (i - 1)) && (name[i] != name[i - 1]))) {
+                    for (d in i..name.size - 1) {
+                        if ((name.lastIndex > d) && (name[i] == name[d + 1])) {
+                            count++
+                        } else {
+                            continue
+                        }
+                    }
+                }
+                if (count > 1) {
+                    countName += name[i] + "$count"
+                }
+            }
+            println(countName)
+        }
 }
